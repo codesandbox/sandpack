217a5d5197e770090b5aae5eed221f811bd35a81
Jasper De Moor <jasperdemoor@gmail.com>
2021-04-20 12:02:39 +0200

    tweak

--- sandpack-playground/src/App.tsx
+++ sandpack-playground/src/App.tsx
@@ -1,19 +1,30 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
 import React from "react";
 
+const reactCode = `export default function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+`;
+
 export const App: React.FC = () => {
   return (
-    <Sandpack
-      files={{
-        "/App.js": "",
-      }}
-      options={{
-        autorun: false,
-        showTabs: true,
-        showLineNumbers: true,
-        showNavigator: true,
-      }}
-      template="react"
-    />
+    <div style={{ height: "100vh", width: "100%" }}>
+      <Sandpack
+        files={{
+          "/App.js": reactCode,
+        }}
+        options={{
+          autorun: true,
+          showTabs: true,
+          showLineNumbers: true,
+          showNavigator: true,
+        }}
+        template="react"
+      />
+    </div>
   );
 };
