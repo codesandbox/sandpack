1d78b0ff0b5b011c2a2c781e08be00dc66ce5584
Danilo Woznica <danilowoz@gmail.com>
2021-06-29 14:59:23 +0100

    Revert "build(client): standard the build dest"

--- sandpack-client/gulpfile.js
+++ sandpack-client/gulpfile.js
@@ -1,10 +1,12 @@
-import { task, src, dest } from "gulp";
+const gulp = require('gulp');
 
-task("copy-sandbox", () =>
-  src([
-    "../../codesandbox-client/www/**/*.*",
-    "!../../codesandbox-client/www/**/*.map",
-    "!../../codesandbox-client/www/stats.json",
-    "!../../codesandbox-client/www/public/**/*.*",
-  ]).pipe(dest("./sandpack/"))
+gulp.task('copy-sandbox', () =>
+  gulp
+    .src([
+      '../../codesandbox-client/www/**/*.*',
+      '!../../codesandbox-client/www/**/*.map',
+      '!../../codesandbox-client/www/stats.json',
+      '!../../codesandbox-client/www/public/**/*.*',
+    ])
+    .pipe(gulp.dest('./sandpack/'))
 );
