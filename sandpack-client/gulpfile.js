a6c555a75275b8578fb2d6d2c4818ba82bdb30d3
Danilo Woznica <danilowoz@gmail.com>
2021-06-29 15:03:53 +0100

    chore(sabdpack client): bump version

--- sandpack-client/gulpfile.js
+++ sandpack-client/gulpfile.js
@@ -1,12 +1,10 @@
-const gulp = require('gulp');
+import { task, src, dest } from "gulp";
 
-gulp.task('copy-sandbox', () =>
-  gulp
-    .src([
-      '../../codesandbox-client/www/**/*.*',
-      '!../../codesandbox-client/www/**/*.map',
-      '!../../codesandbox-client/www/stats.json',
-      '!../../codesandbox-client/www/public/**/*.*',
-    ])
-    .pipe(gulp.dest('./sandpack/'))
+task("copy-sandbox", () =>
+  src([
+    "../../codesandbox-client/www/**/*.*",
+    "!../../codesandbox-client/www/**/*.map",
+    "!../../codesandbox-client/www/stats.json",
+    "!../../codesandbox-client/www/public/**/*.*",
+  ]).pipe(dest("./sandpack/"))
 );
