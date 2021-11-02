b4196941e67cbd5f72739a48ded029d1b1eb3b84
Maria Clara <claarasantana@gmail.com>
2021-11-02 12:57:54 -0300

    chore: bootstrap project with create-next-app

--- website/landing/pages/api/hello.ts
+++ website/landing/pages/api/hello.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-interface Data {
+type Data = {
   name: string;
-}
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
-): void {
+) {
   res.status(200).json({ name: "John Doe" });
 }
