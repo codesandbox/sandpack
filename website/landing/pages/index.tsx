e6a1ac217024ac7339ad74dd821abb666ffb57e1
Maria Clara <maria@codesandbox.io>
2021-11-09 16:02:41 -0300

    feat(home page): uses content from global config file

--- website/landing/pages/index.tsx
+++ website/landing/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+
 import { Api } from "../components/Api";
 import { Banner } from "../components/Banner";
 import { Community } from "../components/Community";
@@ -10,8 +11,8 @@ import { Hero } from "../components/Hero";
 import { Intro } from "../components/Intro";
 import { Showcase } from "../components/Showcase";
 import { Users } from "../components/Users";
-import { content } from "../content/global";
 import { styled } from "../stitches.config";
+import content from "../website.config.json";
 
 const Container = styled("section", {
   display: "flex",
@@ -28,8 +29,8 @@ const Home: NextPage = () => {
   return (
     <Container>
       <Head>
-        <title>{content.pageTitle}</title>
-        <meta content={content.pageDescription} name="description" />
+        <title>{content.global.title}</title>
+        <meta content={content.global.description} name="description" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
 
