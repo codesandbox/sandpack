4b38c02eaa4012c79e048f37c0f8bfb6ceb28801
Danilo Woznica <danilowoz@gmail.com>
2021-11-15 11:04:21 +0000

    fix(react): split bundle modules

--- sandpack-react/build.js
+++ sandpack-react/build.js
@@ -1,39 +1,35 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require("fs");
 const { build } = require("esbuild");
+const glob = require("glob");
+
 const package = require("./package.json");
 
-const options = {
-  entryPoints: fs
-    .readdirSync("./src")
-    .filter((src) => src.endsWith(".ts"))
-    .map((e) => `src/${e}`),
+const buildProject = async () => {
+  const getFiles = () =>
+    new Promise((resolve) => {
+      glob("./src/**/index.ts[x]", {}, (err, files) => {
+        resolve(["./src/index.ts", ...files]);
+      });
+    });
 
-  bundle: true,
-  sourcemap: true,
-  external: Object.keys({
-    ...(package.dependencies || {}),
-    ...(package.devDependencies || {}),
-    ...(package.peerDependencies || {}),
-  }),
+  build({
+    entryPoints: await getFiles(),
+    entryNames: "[dir]/[name]",
+    outbase: "src",
+    bundle: true,
+    sourcemap: true,
+    external: Object.keys({
+      ...(package.dependencies || {}),
+      ...(package.devDependencies || {}),
+      ...(package.peerDependencies || {}),
+    }),
+    format: "esm",
+    outdir: "dist",
+    target: "es2019",
+  }).catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 };
 
-build({
-  ...options,
-  format: "esm",
-  outdir: "dist/esm",
-  target: "es2019",
-}).catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
-
-build({
-  ...options,
-  format: "cjs",
-  outdir: "dist/cjs",
-  target: "es6",
-}).catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+buildProject();
