38e82e12e1f5fa332d7e02383a9146d7063b3876
Maria Clara <claarasantana@gmail.com>
2021-11-19 02:01:55 +0000

    workaround

--- website/landing/components/AdvancedUsage/AdvancedUsage.tsx
+++ website/landing/components/AdvancedUsage/AdvancedUsage.tsx
@@ -1,7 +1,7 @@
 import { useSpring, useTransform, useViewportScroll } from "framer-motion";
 import React, { useLayoutEffect, useRef, useState } from "react";
 
-import config from "../../website.config.json";
+// import config from "../../website.config.json";
 import {
   List,
   ListItem,
@@ -17,7 +17,31 @@ import { UsageExample } from "./UsageExample";
 const SPRING_OPTIONS = { stiffness: 50, damping: 10 };
 
 export const AdvancedUsage: React.FC = () => {
-  const content = config.advancedUsage;
+  // TODO: investigate why the app isn't able to find the existing
+  // content from the website config.
+  const content = {
+    title: "Building blocks<br />of the Sandpack<br />ecosystem.",
+    examples: [
+      {
+        title: "Providers",
+        description:
+          "We export a <code>SandpackProvider</code> that manages the state and actions for sandpack and a <code>ThemeProvider</code> that handles the theme and style.",
+        illustrationKey: "providers",
+      },
+      {
+        title: "Components",
+        description:
+          "We also export the smaller parts that make up the <code>&lt;Sandpack /&gt;</code> component, so you can combine them with your custom components.",
+        illustrationKey: "components",
+      },
+      {
+        title: "Client",
+        description:
+          "The bundler is wrapped by a small package called <code>sandpack-client</code>, which is framework agnostic and allows you to tap into the bundler protocol.",
+        illustrationKey: "client",
+      },
+    ],
+  };
 
   const section = useRef<HTMLDivElement>(null);
   const [sectionTop, setSectionTop] = useState(0);
