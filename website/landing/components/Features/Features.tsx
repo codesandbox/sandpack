48e777964fe0757b569c934845939cc286fd4ee4
Maria Clara <maria@codesandbox.io>
2021-11-19 12:17:56 -0300

    feat: update features to use section and card

--- website/landing/components/Features/Features.tsx
+++ website/landing/components/Features/Features.tsx
@@ -1,163 +1,93 @@
-import content from "../../website.config.json";
-import { Box, List, Text } from "../common";
+import config from "../../website.config.json";
+import {
+  Box,
+  Card,
+  CardDescription,
+  CardTitle,
+  List,
+  ListItem,
+  SectionContainer,
+  SectionHeader,
+  SectionTitle,
+  SectionWrapper,
+} from "../common";
 
 import { ICONS } from "./icons";
 
 export const Features: React.FC = () => {
-  const { features } = content;
+  const content = config.features;
 
   return (
-    <Box
-      as="section"
-      css={{
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "column",
-        gap: "100px",
-        justifyContent: "center",
-        padding: "200px 16px",
-
-        "@bp1": {
-          gap: "200px",
-        },
-
-        "@bp2": {
-          maxWidth: "1200px",
-        },
-      }}
-    >
-      <Text
-        as="h2"
+    <SectionWrapper>
+      <SectionContainer
         css={{
-          fontSize: "36px",
-          fontWeight: "$semiBold",
-          letterSpacing: "-0.05em",
-          lineHeight: "1",
-          textAlign: "center",
-
-          "@bp1": {
-            fontSize: "64px",
-          },
-
           "@bp2": {
-            alignSelf: "flex-start",
-            fontSize: "72px",
-            textAlign: "start",
-          },
-
-          "@bp3": {
-            fontSize: "96px",
-          },
-        }}
-        dangerouslySetInnerHTML={{ __html: features.title }}
-      />
-      <List
-        css={{
-          alignItems: "center",
-          display: "flex",
-          flexDirection: "column",
-          gap: "120px",
-          width: "100%",
-
-          "@bp2": {
-            alignItems: "flex-start",
-            flexDirection: "row",
-            justifyContent: "space-between",
+            maxWidth: "65%",
           },
         }}
       >
-        {features.highlights.map((h, index) => {
-          const icon = ICONS[h.iconKey as keyof typeof ICONS];
+        <SectionHeader css={{ "@bp2": { alignItems: "flex-start" } }}>
+          <SectionTitle
+            css={{ "@bp2": { textAlign: "start" } }}
+            dangerouslySetInnerHTML={{ __html: content.title }}
+          />
+        </SectionHeader>
+        <List
+          css={{
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            gap: "48px",
+            justifyContent: "center",
+            transition: "opacity .5s cubic-bezier(0.770, 0.000, 0.175, 1.000)",
 
-          return (
-            <Box
-              key={`section-features-${index}`}
-              as="li"
-              css={{
-                alignItems: "center",
-                display: "flex",
-                flexDirection: "column",
-                gap: "32px",
-                width: "256px",
+            "@bp1": {
+              gap: "100px",
+            },
 
-                "@bp2": {
-                  alignItems: "flex-start",
-                  width: "240px",
-                },
+            "@bp2": {
+              alignItems: "flex-start",
+              flexDirection: "row",
+              flexFlow: "row wrap",
+              margin: "100px 0 200px",
+              justifyContent: "space-between",
+            },
+          }}
+        >
+          {content.highlights.map((h, index) => {
+            const icon = ICONS[h.iconKey as keyof typeof ICONS];
 
-                "@bp3": {
-                  width: "320px",
-                },
-              }}
-            >
-              <Box
-                css={{
-                  alignItems: "center",
-                  color: "$lightTextPrimary",
-                  background: "$primary",
-                  borderRadius: "100%",
-                  display: "flex",
-                  flexShrink: "0",
-                  flexGrow: "0",
-                  height: "78px",
-                  justifyContent: "center",
-                  padding: "18px",
-                  width: "78px",
-                }}
-              >
-                {icon}
-              </Box>
-              <Box
-                css={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "12px",
-                }}
-              >
-                <Text
-                  css={{
-                    fontSize: "24px",
-                    fontWeight: "$normal",
-                    letterSpacing: "-0.05em",
-                    lineHeight: "1.2",
-                    textAlign: "center",
-
-                    "@bp1": {
-                      fontSize: "36px",
-                    },
-
-                    "@bp2": {
-                      textAlign: "start",
-                    },
-
-                    "@bp3": {
-                      fontSize: "24px",
-                    },
-                  }}
-                >
-                  {h.title}
-                </Text>
-                <Text
-                  css={{
-                    color: "$darkTextSecondary",
-                    fontSize: "16px",
-                    lineHeight: "1.4",
-                    letterSpacing: "-0.05em",
-                    textAlign: "center",
-
-                    "@bp2": {
-                      fontSize: "18px",
-                      textAlign: "start",
-                    },
-                  }}
-                >
-                  {h.description}
-                </Text>
-              </Box>
-            </Box>
-          );
-        })}
-      </List>
-    </Box>
+            return (
+              <ListItem key={`content-highlight-${index}`}>
+                <Card size="small">
+                  <Box
+                    css={{
+                      alignItems: "center",
+                      color: "$lightTextPrimary",
+                      background: "$primary",
+                      borderRadius: "100%",
+                      display: "flex",
+                      flexShrink: "0",
+                      flexGrow: "0",
+                      height: "78px",
+                      justifyContent: "center",
+                      marginBottom: "20px",
+                      padding: "18px",
+                      width: "78px",
+                    }}
+                  >
+                    {icon}
+                  </Box>
+                  <CardTitle size="small">{h.title}</CardTitle>
+                  <CardDescription size="small">
+                    {h.description}
+                  </CardDescription>
+                </Card>
+              </ListItem>
+            );
+          })}
+        </List>
+      </SectionContainer>
+    </SectionWrapper>
   );
 };
