33c2499ae10eb216fa0d1b14dc3818854243e7df
Maria Clara <claarasantana@gmail.com>
2021-11-19 13:50:09 -0300

    chore: landing - refine features section (#161)
    * feat: support highlight class within section titles
    
    * fix: adjust section margin/padding
    
    * fix: remove unused useeffect from users
    
    * feat: adds small variant to card component
    
    * feat: update features to use section and card
    
    * feat: animate features

--- website/landing/components/Features/Features.tsx
+++ website/landing/components/Features/Features.tsx
@@ -1,163 +1,149 @@
-import content from "../../website.config.json";
-import { Box, List, Text } from "../common";
+import type { MotionValue } from "framer-motion";
+import { motion, useTransform, useViewportScroll } from "framer-motion";
+import { useMemo } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
+
+import config from "../../website.config.json";
+import {
+  Box,
+  Card,
+  CardDescription,
+  CardTitle,
+  List,
+  ListItem,
+  SectionContainer,
+  SectionHeader,
+  SectionTitle,
+  SectionWrapper,
+} from "../common";
 
 import { ICONS } from "./icons";
 
+interface FeatureFadeProps {
+  index: number;
+  parentTop: number;
+  parentHeight: number;
+  scrollY: MotionValue<number>;
+}
+const FeatureFade: React.FC<FeatureFadeProps> = ({
+  children,
+  index,
+  parentTop,
+  parentHeight,
+  scrollY,
+}) => {
+  const opacityRange = [0, 1];
+  const scrollRange = [
+    parentTop - parentHeight / (2 * Math.pow(index + 1, 2)), // Stagger elements by their index.
+    parentTop + parentHeight / (index === 0 ? 6 : 4), // The title (index === 0) should have opacity 1 sooner than the list elements.
+  ];
+  const opacity = useTransform(scrollY, scrollRange, opacityRange);
+
+  return <motion.div style={{ opacity }}>{children}</motion.div>;
+};
+
 export const Features: React.FC = () => {
-  const { features } = content;
+  const content = config.features;
+
+  const { scrollY } = useViewportScroll();
+
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const [sectionTop, setSectionTop] = useState(0);
+  const [sectionHeight, setSectionHeight] = useState(0);
+
+  useLayoutEffect(() => {
+    const sectionEl = sectionRef.current;
+    if (!sectionEl) return;
+
+    const onResize = () => {
+      setSectionTop(sectionEl.offsetTop);
+      setSectionHeight(sectionEl.offsetHeight);
+    };
+
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, [sectionRef]);
+
+  const fadeProps = useMemo(
+    () => ({ scrollY, parentTop: sectionTop, parentHeight: sectionHeight }),
+    [scrollY, sectionTop, sectionHeight]
+  );
 
   return (
-    <Box
-      as="section"
-      css={{
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "column",
-        gap: "100px",
-        justifyContent: "center",
-        padding: "200px 16px",
-
-        "@bp1": {
-          gap: "200px",
-        },
-
-        "@bp2": {
-          maxWidth: "1200px",
-        },
-      }}
-    >
-      <Text
-        as="h2"
+    <SectionWrapper ref={sectionRef}>
+      <SectionContainer
         css={{
-          fontSize: "36px",
-          fontWeight: "$semiBold",
-          letterSpacing: "-0.05em",
-          lineHeight: "1",
-          textAlign: "center",
-
-          "@bp1": {
-            fontSize: "64px",
-          },
-
           "@bp2": {
-            alignSelf: "flex-start",
-            fontSize: "72px",
-            textAlign: "start",
-          },
-
-          "@bp3": {
-            fontSize: "96px",
-          },
-        }}
-        dangerouslySetInnerHTML={{ __html: features.title }}
-      />
-      <List
-        css={{
-          alignItems: "center",
-          display: "flex",
-          flexDirection: "column",
-          gap: "120px",
-          width: "100%",
-
-          "@bp2": {
-            alignItems: "flex-start",
-            flexDirection: "row",
-            justifyContent: "space-between",
+            maxWidth: "65%",
           },
         }}
       >
-        {features.highlights.map((h, index) => {
-          const icon = ICONS[h.iconKey as keyof typeof ICONS];
+        <FeatureFade index={0} {...fadeProps}>
+          <SectionHeader css={{ "@bp2": { alignItems: "flex-start" } }}>
+            <SectionTitle
+              css={{ "@bp2": { textAlign: "start" } }}
+              dangerouslySetInnerHTML={{ __html: content.title }}
+            />
+          </SectionHeader>
+        </FeatureFade>
+        <List
+          css={{
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            gap: "48px",
+            justifyContent: "center",
+            transition: "opacity .5s cubic-bezier(0.770, 0.000, 0.175, 1.000)",
 
-          return (
-            <Box
-              key={`section-features-${index}`}
-              as="li"
-              css={{
-                alignItems: "center",
-                display: "flex",
-                flexDirection: "column",
-                gap: "32px",
-                width: "256px",
+            "@bp1": {
+              gap: "100px",
+            },
 
-                "@bp2": {
-                  alignItems: "flex-start",
-                  width: "240px",
-                },
+            "@bp2": {
+              alignItems: "flex-start",
+              flexDirection: "row",
+              margin: "100px 0 200px",
+              justifyContent: "space-between",
+            },
+          }}
+        >
+          {content.highlights.map((highlight, highlightIndex) => {
+            const icon = ICONS[highlight.iconKey as keyof typeof ICONS];
 
-                "@bp3": {
-                  width: "320px",
-                },
-              }}
-            >
-              <Box
-                css={{
-                  alignItems: "center",
-                  color: "$lightTextPrimary",
-                  background: "$primary",
-                  borderRadius: "100%",
-                  display: "flex",
-                  flexShrink: "0",
-                  flexGrow: "0",
-                  height: "78px",
-                  justifyContent: "center",
-                  padding: "18px",
-                  width: "78px",
-                }}
-              >
-                {icon}
-              </Box>
-              <Box
-                css={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "12px",
-                }}
-              >
-                <Text
-                  css={{
-                    fontSize: "24px",
-                    fontWeight: "$normal",
-                    letterSpacing: "-0.05em",
-                    lineHeight: "1.2",
-                    textAlign: "center",
-
-                    "@bp1": {
-                      fontSize: "36px",
-                    },
-
-                    "@bp2": {
-                      textAlign: "start",
-                    },
-
-                    "@bp3": {
-                      fontSize: "24px",
-                    },
-                  }}
-                >
-                  {h.title}
-                </Text>
-                <Text
-                  css={{
-                    color: "$darkTextSecondary",
-                    fontSize: "16px",
-                    lineHeight: "1.4",
-                    letterSpacing: "-0.05em",
-                    textAlign: "center",
-
-                    "@bp2": {
-                      fontSize: "18px",
-                      textAlign: "start",
-                    },
-                  }}
-                >
-                  {h.description}
-                </Text>
-              </Box>
-            </Box>
-          );
-        })}
-      </List>
-    </Box>
+            return (
+              <ListItem key={`content-highlight-${highlightIndex}`}>
+                <FeatureFade index={highlightIndex + 1} {...fadeProps}>
+                  <Card size="small">
+                    <Box
+                      css={{
+                        alignItems: "center",
+                        color: "$lightTextPrimary",
+                        background: "$primary",
+                        borderRadius: "100%",
+                        display: "flex",
+                        flexShrink: "0",
+                        flexGrow: "0",
+                        height: "78px",
+                        justifyContent: "center",
+                        marginBottom: "20px",
+                        padding: "18px",
+                        width: "78px",
+                      }}
+                    >
+                      {icon}
+                    </Box>
+                    <CardTitle size="small">{highlight.title}</CardTitle>
+                    <CardDescription size="small">
+                      {highlight.description}
+                    </CardDescription>
+                  </Card>
+                </FeatureFade>
+              </ListItem>
+            );
+          })}
+        </List>
+      </SectionContainer>
+    </SectionWrapper>
   );
 };
