03beaa343f2ea3daa75dd33b8b1054f41f1b3706
Maria Clara <claarasantana@gmail.com>
2021-11-24 14:01:46 +0000

    feat: update Showcase to use Section and Card shared elements

--- website/landing/components/Showcase/Showcase.tsx
+++ website/landing/components/Showcase/Showcase.tsx
@@ -1,5 +1,17 @@
 import content from "../../website.config.json";
-import { Box, List, Text } from "../common";
+import {
+  Box,
+  List,
+  ListItem,
+  SectionWrapper,
+  SectionContainer,
+  SectionHeader,
+  SectionTitle,
+  Card,
+  CardTitle,
+  CardDescription,
+  Text,
+} from "../common";
 
 const ContentLandmark = () => {
   return (
@@ -38,133 +50,74 @@ export const Showcase: React.FC = () => {
   const { showCase } = content;
 
   return (
-    <Box
-      as="section"
-      css={{
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "column",
-        gap: "200px",
-        justifyContent: "center",
-        padding: "200px 16px",
+    <SectionWrapper>
+      <SectionContainer>
+        <SectionHeader>
+          <SectionTitle dangerouslySetInnerHTML={{ __html: showCase.title }} />
+        </SectionHeader>
+        <List
+          css={{
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            gap: "148px",
+            width: "100%",
 
-        "@bp2": {
-          padding: "200px 0 0",
-        },
-      }}
-    >
-      <Text
-        as="h2"
-        css={{
-          fontSize: "36px",
-          fontWeight: "$semiBold",
-          letterSpacing: "-0.05em",
-          lineHeight: "1",
-          textAlign: "center",
-
-          "@bp1": {
-            fontSize: "72px",
-          },
-
-          "@bp2": {
-            fontSize: "96px",
-          },
-
-          "@bp3": {
-            fontSize: "144px",
-          },
-        }}
-        dangerouslySetInnerHTML={{ __html: showCase.title }}
-      />
-      <List
-        css={{
-          alignItems: "center",
-          display: "flex",
-          flexDirection: "column",
-          gap: "148px",
-          width: "100%",
-
-          "@bp2": {
-            display: "grid",
-            gap: "200px",
-            gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
-          },
-        }}
-      >
-        {showCase.highlights.map((h, index) => (
-          <Box
-            key={`section-showcase-${index}`}
-            as="li"
-            css={{
-              alignItems: "center",
-              display: "flex",
-              flexDirection: "column",
-              gap: "40px",
-
-              "@bp1": {
-                width: "384px",
-              },
-
-              "@bp2": {
-                width: "360px",
-
-                "&:nth-of-type(odd)": {
-                  justifySelf: "flex-end",
-                },
-
-                "&:ntt-of-type(even)": {
-                  justifySelf: "flex-start",
-                },
-              },
-
-              "@bp3": {
-                width: "480px",
-              },
-            }}
-          >
-            <ContentLandmark />
-            <Box
+            "@bp2": {
+              display: "grid",
+              gap: "200px",
+              gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+            },
+          }}
+        >
+          {showCase.highlights.map((h, index) => (
+            <ListItem
+              key={`section-showcase-${index}`}
               css={{
                 alignItems: "center",
                 display: "flex",
                 flexDirection: "column",
-                gap: "24px",
+                gap: "40px",
+
+                "@bp1": {
+                  width: "384px",
+                },
+
+                "@bp2": {
+                  width: "360px",
+
+                  "&:nth-of-type(odd)": {
+                    justifySelf: "flex-end",
+                  },
+
+                  "&:ntt-of-type(even)": {
+                    justifySelf: "flex-start",
+                  },
+                },
+
+                "@bp3": {
+                  width: "480px",
+                },
               }}
             >
-              <Text
-                css={{
-                  fontSize: "24px",
-                  fontWeight: "$semiBold",
-                  letterSpacing: "-0.05em",
-                  lineHeight: "1.2",
-                  textAlign: "center",
-
-                  "@bp1": {
-                    fontSize: "36px",
-                  },
-                }}
-              >
-                {h.title}
-              </Text>
-              <Text
-                css={{
-                  color: "$darkTextSecondary",
-                  fontSize: "16px",
-                  lineHeight: "1.4",
-                  letterSpacing: "-0.025em",
-                  textAlign: "center",
-
-                  "@bp2": {
-                    fontSize: "18px",
-                  },
-                }}
-              >
-                {h.description}
-              </Text>
-            </Box>
-          </Box>
-        ))}
-      </List>
-    </Box>
+              <ContentLandmark />
+              <Card css={{ alignItems: "center" }}>
+                <CardTitle
+                  dangerouslySetInnerHTML={{
+                    __html: h.title,
+                  }}
+                />
+                <CardDescription
+                  css={{ textAlign: "center" }}
+                  dangerouslySetInnerHTML={{
+                    __html: h.description,
+                  }}
+                />
+              </Card>
+            </ListItem>
+          ))}
+        </List>
+      </SectionContainer>
+    </SectionWrapper>
   );
 };
