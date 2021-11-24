0a0719958ea0b935f513fd928d8101b297e2d60b
Maria Clara <maria@codesandbox.io>
2021-11-24 16:57:38 -0300

    screen reader variant for Text common component

--- website/landing/components/common/Text.ts
+++ website/landing/components/common/Text.ts
@@ -20,4 +20,21 @@ export const Text = styled("p", {
   ".highlight": {
     color: "$primary",
   },
+
+  variants: {
+    screenReader: {
+      true: {
+        border: "0 !important",
+        clipPath: "inset(50%) !important",
+        "-webkit-clip-path": "inset(50%) !important",
+        height: "1px !important",
+        margin: "-1px !important",
+        overflow: "hidden !important",
+        padding: "0 !important",
+        position: "absolute !important",
+        width: "1px !important",
+        whiteSpace: "nowrap !important",
+      },
+    },
+  },
 });
