82ef1f5ab52cdeb658d1565a87c6961471226871
Maria Clara <maria@codesandbox.io>
2021-11-24 17:35:04 -0300

    update button override

--- website/landing/components/common/Button.tsx
+++ website/landing/components/common/Button.tsx
@@ -3,7 +3,11 @@ import { styled } from "../../stitches.config";
 export const Button = styled("button", {
   background: "none",
   border: "none",
-  boxShadow: "none",
-  margin: "0",
-  padding: "0",
+  borderRradius: 0,
+  color: "inherit",
+  font: "inherit",
+  lineHeight: "normal",
+  margin: 0,
+  padding: 0,
+  width: "auto",
 });
