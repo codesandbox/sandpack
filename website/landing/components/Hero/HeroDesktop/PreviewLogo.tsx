4742c1bc47e09d55135a7531781e9eb0f162ef47
Maria Clara <maria@codesandbox.io>
2021-11-24 23:29:10 -0300

    wip

---
+++ website/landing/components/Hero/HeroDesktop/PreviewLogo.tsx
@@ -0,0 +1,21 @@
+export const PreviewLogo: React.FC = () => {
+  return (
+    <svg
+      fill="none"
+      height="100%"
+      viewBox="0 0 178 217"
+      width="100%"
+    >
+      <path
+        d="M89.222 48h81.867v162.096H89.222z"
+        stroke="#fff"
+        strokeWidth="13.5"
+      />
+      <path
+        d="M7.5 7.128h81.867v162.096H7.5z"
+        stroke="#fff"
+        strokeWidth="13.5"
+      />
+    </svg>
+  );
+};
