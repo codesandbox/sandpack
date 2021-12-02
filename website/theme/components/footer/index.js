4cff80b802e52b94e54d97c1d2b617eaacd23e53
Zeh Fernandes <ozehfernandes@gmail.com>
2021-12-02 15:01:08 +0000

    hot fix route problem styles

--- website/theme/components/footer/index.js
+++ website/theme/components/footer/index.js
@@ -4,14 +4,15 @@ export default function Footer({ ...props }) {
   return (
     <footer className={styles.container} {...props}>
       <p className={styles.warning}>
-        Read more about how to implement Sandpack on your project on{" "}
+        Learn how to implement Sandpack in your project on{" "}
         <a href="#" target="_blank">
           <u>Sandpack Documentation</u>
         </a>
         .
       </p>
       <div className={styles.logoContainer}>
-        Powered by <img className={styles.logo} src="theme/codesandbox-logo.svg" />
+        Powered by{" "}
+        <img className={styles.logo} src="theme/codesandbox-logo.svg" />
       </div>
     </footer>
   );
