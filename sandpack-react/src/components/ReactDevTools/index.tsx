2cbe9742580bad4326233ab526e272c8e8a02c47
Danilo Woznica <danilowoz@gmail.com>
2021-12-08 19:59:37 +0000

    import approach

--- sandpack-react/src/components/ReactDevTools/index.tsx
+++ sandpack-react/src/components/ReactDevTools/index.tsx
@@ -1,5 +1,5 @@
 import { useClasser } from "@code-hike/classer";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { useSandpackTheme } from "../..";
 import { useSandpack } from "../../hooks/useSandpack";
@@ -11,22 +11,28 @@ export const SandpackReactDevTools: React.FC<{ clientId?: string }> = ({
   const { listen, sandpack } = useSandpack();
   const { theme } = useSandpackTheme();
   const c = useClasser("sp");
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const reactDevtools = useRef<any>();
 
   const [ReactDevTools, setDevTools] = useState<React.FunctionComponent<{
     browserTheme: "dark" | "light";
   }> | null>(null);
 
   useEffect(() => {
-    const unsubscribe = listen(async (msg) => {
-      if (msg.type === "activate-react-devtools") {
-        const reactDevtools = await import("react-devtools-inline/frontend");
+    import("react-devtools-inline/frontend").then((module) => {
+      reactDevtools.current = module;
+    });
+  }, []);
 
+  useEffect(() => {
+    const unsubscribe = listen((msg) => {
+      if (msg.type === "activate-react-devtools") {
         const client = clientId
           ? sandpack.clients[clientId]
           : Object.values(sandpack.clients)[0];
         const contentWindow = client?.iframe?.contentWindow;
 
-        setDevTools(reactDevtools.initialize(contentWindow));
+        setDevTools(reactDevtools.current.initialize(contentWindow));
       }
     });
 
