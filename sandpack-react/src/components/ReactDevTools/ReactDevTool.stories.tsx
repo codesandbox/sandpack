a1ca04af811f1cd08e02f908145e0b9029c729b1
Danilo Woznica <danilowoz@gmail.com>
2021-12-15 10:20:06 +0000

    multiples devtools: wip

--- sandpack-react/src/components/ReactDevTools/ReactDevTool.stories.tsx
+++ sandpack-react/src/components/ReactDevTools/ReactDevTool.stories.tsx
@@ -7,7 +7,12 @@ export default {
   title: "components/ReactDevTools",
 };
 
-export const ReactDevTool: React.FC = () => (
+// Bug
+// https://github.com/codesandbox/codesandbox-client/pull/5779
+
+// How to implemented it
+// https://github.com/facebook/react/blob/main/packages/react-devtools-inline/README.md#advanced-integration-with-custom-wall
+const ReactDevTool = (): JSX.Element => (
   <SandpackProvider
     customSetup={{
       files: {
@@ -40,3 +45,15 @@ return (
     </SandpackThemeProvider>
   </SandpackProvider>
 );
+
+export { ReactDevTool };
+
+export const MultipleReactDevTool = (): JSX.Element => {
+  return (
+    <>
+      <ReactDevTool />
+      <ReactDevTool />
+      <ReactDevTool />
+    </>
+  );
+};
