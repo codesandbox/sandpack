052a03a7f83ecbb351cadbaa5ccd2a2b5b705fdc
Danilo Woznica <danilowoz@gmail.com>
2021-12-16 13:44:14 +0000

    improve

--- cypress/integration/CodeViewer.spec.js
+++
@@ -1,9 +0,0 @@
-describe("CodeViewer", () => {
-  it("should render a decoration component properly", () => {
-    cy.viewport(1200, 1500).visit(
-      `/iframe.html?id=components-code-viewer--decorators`
-    );
-
-    cy.get(".cm-content").invoke("attr", "style", "tab-size: 4;").snapshot();
-  });
-});
