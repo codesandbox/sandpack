d42fb4239becdb7353f63917b20fc7d135e07cef
Danilo Woznica <danilowoz@gmail.com>
2021-12-20 11:52:52 +0000

    format

--- sandpack-react/src/components/FileTabs/FileTabs.test.tsx
+++ sandpack-react/src/components/FileTabs/FileTabs.test.tsx
@@ -5,27 +5,27 @@ import { SandpackProvider } from "../../contexts/sandpackContext";
 import { SandpackCodeEditor } from "../CodeEditor";
 
 describe("FileTabs", () => {
-    it("doesn't have duplicate filename", () => {
-        const component = create(
-            <SandpackProvider
-                template="react"
-                customSetup={{
-                    files: {
-                        "/foo/App.js": "",
-                        "/App.js": "",
-                        "/baz/App.js": "",
-                    },
-                }}
-            >
-                <SandpackCodeEditor />
-            </SandpackProvider>
-        ).root;
+  it("doesn't have duplicate filename", () => {
+    const component = create(
+      <SandpackProvider
+        template="react"
+        customSetup={{
+          files: {
+            "/foo/App.js": "",
+            "/App.js": "",
+            "/baz/App.js": "",
+          },
+        }}
+      >
+        <SandpackCodeEditor />
+      </SandpackProvider>
+    ).root;
 
-        const buttons = component.findAllByProps({
-            className: "sp-tab-button",
-        });
-        const buttonsTex = buttons.map((item) => item.props.children[0]);
-
-        expect(buttonsTex).toEqual(["foo/App.js", "App.js", "baz/App.js"]);
+    const buttons = component.findAllByProps({
+      className: "sp-tab-button",
     });
+    const buttonsTex = buttons.map((item) => item.props.children[0]);
+
+    expect(buttonsTex).toEqual(["foo/App.js", "App.js", "baz/App.js"]);
+  });
 });
