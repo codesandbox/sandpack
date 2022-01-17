d00074241e5cbd7dbfd1bf7c6f41dea411f24f11
Danilo Woznica <danilowoz@gmail.com>
2022-01-17 14:19:57 +0000

    ssr: examples (#277)

---
+++ examples/gatsby/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Sandpack } from "@codesandbox/sandpack-react";
+
+const Home = () => {
+  return (
+    <div>
+      <Sandpack />
+      <Sandpack theme="sandpack-dark" />
+      <Sandpack theme="github-light" />
+    </div>
+  );
+};
+
+export default Home;
