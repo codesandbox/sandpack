9155a1e36b380113533b6db54272b8b06a4ab5fa
Danilo Woznica <danilowoz@gmail.com>
2022-01-27 10:05:05 +0000

    fix(codemirror): adds language extension as classname (#334)

--- sandpack-react/src/components/CodeEditor/CodeMirror.tsx
+++ sandpack-react/src/components/CodeEditor/CodeMirror.tsx
@@ -35,6 +35,7 @@ import { highlightDecorators } from "./highlightDecorators";
 import { highlightInlineError } from "./highlightInlineError";
 import {
   getCodeMirrorLanguage,
+  getLanguageFromFile,
   getEditorTheme,
   getSyntaxHighlight,
   useCombinedRefs,
@@ -143,6 +144,9 @@ export const CodeMirror = React.forwardRef<CodeMirrorRef, CodeMirrorProps>(
       }
     }, [initMode, isIntersecting]);
 
+    const languageExtension = getLanguageFromFile(filePath, fileType);
+    const langSupport = getCodeMirrorLanguage(languageExtension);
+
     React.useEffect(() => {
       if (!wrapper.current || !shouldInitEditor) return;
 
@@ -151,8 +155,6 @@ export const CodeMirror = React.forwardRef<CodeMirrorRef, CodeMirrorProps>(
        * waiting for "postTask scheduler" API be ready
        */
       const timer = setTimeout(function delayCodeEditorInit() {
-        const langSupport = getCodeMirrorLanguage(filePath, fileType);
-
         const customCommandsKeymap: KeyBinding[] = [
           {
             key: "Tab",
@@ -358,7 +360,11 @@ export const CodeMirror = React.forwardRef<CodeMirrorRef, CodeMirrorProps>(
 
     if (readOnly) {
       return (
-        <pre ref={combinedRef} className={c("cm", editorState)} translate="no">
+        <pre
+          ref={combinedRef}
+          className={c("cm", editorState, languageExtension)}
+          translate="no"
+        >
           <code className={c("pre-placeholder")}>{code}</code>
 
           {readOnly && showReadOnly && (
@@ -377,7 +383,7 @@ export const CodeMirror = React.forwardRef<CodeMirrorRef, CodeMirrorProps>(
         aria-label={
           filePath ? `Code Editor for ${getFileName(filePath)}` : `Code Editor`
         }
-        className={c("cm", editorState)}
+        className={c("cm", editorState, languageExtension)}
         onKeyDown={handleContainerKeyDown}
         role="group"
         tabIndex={0}
