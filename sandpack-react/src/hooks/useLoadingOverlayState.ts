19dc2809297e31c6667846cc3ea9201c3ce1f880
Danilo Woznica <danilowoz@gmail.com>
2022-01-31 21:02:48 +0000

    improve state machine

--- sandpack-react/src/hooks/useLoadingOverlayState.ts
+++ sandpack-react/src/hooks/useLoadingOverlayState.ts
@@ -2,7 +2,12 @@ import * as React from "react";
 
 import { useSandpack } from "./useSandpack";
 
-export type LoadingOverlayState = "visible" | "fading" | "hidden" | "timeout";
+export type LoadingOverlayState =
+  | "LOADING"
+  | "PRE_FADING"
+  | "FADING"
+  | "HIDDEN"
+  | "TIMEOUT";
 
 export const FADE_ANIMATION_DURATION = 200;
 
@@ -10,45 +15,64 @@ export const FADE_ANIMATION_DURATION = 200;
  * @category Hooks
  */
 export const useLoadingOverlayState = (
-  clientId?: string
+  clientId?: string,
+  externalLoading?: boolean
 ): LoadingOverlayState => {
   const { sandpack, listen } = useSandpack();
-  const [loadingOverlayState, setLoadingOverlayState] =
-    React.useState<LoadingOverlayState>("visible");
+  const [state, setState] = React.useState<LoadingOverlayState>("LOADING");
 
+  /**
+   * Sandpack listener
+   */
   React.useEffect(() => {
+    // TODO: still using it?
     sandpack.loadingScreenRegisteredRef.current = true;
-    let fadeTimeout: NodeJS.Timer;
 
-    const unsub = listen((message) => {
+    const unsubscribe = listen((message) => {
       if (message.type === "start" && message.firstLoad === true) {
-        setLoadingOverlayState("visible");
+        setState("LOADING");
       }
 
       if (message.type === "done") {
-        setLoadingOverlayState(
-          (prev) => (prev === "visible" ? "fading" : "hidden") // Only set 'fading' if the current state is 'visible'
-        );
-        fadeTimeout = setTimeout(
-          () => setLoadingOverlayState("hidden"),
-          FADE_ANIMATION_DURATION
-        );
+        setState((prev) => {
+          return prev === "LOADING" ? "PRE_FADING" : "HIDDEN";
+        });
       }
     }, clientId);
 
     return (): void => {
-      clearTimeout(fadeTimeout);
-      unsub();
+      unsubscribe();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [clientId, sandpack.status === "idle"]);
 
+  /**
+   * Fading transient state
+   */
+  React.useEffect(() => {
+    let fadeTimeout: NodeJS.Timer;
+
+    if (state === "PRE_FADING" && !externalLoading) {
+      setState("FADING");
+    } else if (state === "FADING") {
+      fadeTimeout = setTimeout(
+        () => setState("HIDDEN"),
+        FADE_ANIMATION_DURATION
+      );
+    }
+
+    return (): void => {
+      clearTimeout(fadeTimeout);
+    };
+  }, [state, externalLoading]);
+
   if (sandpack.status === "timeout") {
-    return "timeout";
+    return "TIMEOUT";
   }
 
   if (sandpack.status !== "running") {
-    return "hidden";
+    return "HIDDEN";
   }
 
-  return loadingOverlayState;
+  return state;
 };
--- sandpack-react/src/presets/Playground.stories.tsx
+++ sandpack-react/src/presets/Playground.stories.tsx
@@ -1,27 +1,36 @@
+import { useEffect, useState } from "react";
+
 import {
   Sandpack,
   SandpackPreview,
   SandpackProvider,
   SandpackLayout,
 } from "../";
+import { LoadingOverlay, SandpackStack } from "../common";
 
 export default {
   title: "Playground",
 };
 
 export const Main = (): JSX.Element => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000);
+  }, []);
+
   return (
     <>
-      {new Array(30).fill(" ").map(() => {
-        return (
-          <SandpackProvider initMode="user-visible">
-            <SandpackLayout>
-              <SandpackPreview />
-            </SandpackLayout>
-          </SandpackProvider>
-        );
-        return <Sandpack options={{ initMode: "lazy" }} />;
-      })}
+      <SandpackProvider initMode="user-visible">
+        <SandpackLayout>
+          <SandpackStack>
+            <div className="sp-preview-container">
+              <LoadingOverlay loading={loading} />
+            </div>
+            <SandpackPreview />
+          </SandpackStack>
+        </SandpackLayout>
+      </SandpackProvider>
     </>
   );
 };
