fcf345e20c3f6b0a3c9f68e453a02d6d102ea1b2
Danilo Woznica <danilowoz@gmail.com>
2022-01-31 15:59:31 +0000

    Update useSandpackNavigation.ts and Sandpack.stories.tsx

--- sandpack-react/src/hooks/useSandpackNavigation.ts
+++ sandpack-react/src/hooks/useSandpackNavigation.ts
@@ -13,10 +13,7 @@ export const useSandpackNavigation = (
   const { dispatch, sandpack } = useSandpack();
 
   return {
-    refresh: (): void => {
-      console.log(sandpack.clients);
-      dispatch({ type: "refresh" }, clientId);
-    },
+    refresh: (): void => dispatch({ type: "refresh" }, clientId),
     back: (): void => dispatch({ type: "urlback" }, clientId),
     forward: (): void => dispatch({ type: "urlforward" }, clientId),
   };
