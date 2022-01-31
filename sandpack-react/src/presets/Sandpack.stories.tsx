53bdb1e61896489a75a6542b5c6d40787ae9cf33
Danilo Woznica <danilowoz@gmail.com>
2022-01-31 11:23:00 +0000

    fix(observer): improve threshold

--- sandpack-react/src/presets/Sandpack.stories.tsx
+++ sandpack-react/src/presets/Sandpack.stories.tsx
@@ -158,7 +158,12 @@ export const InitModeUserVisible: React.FC = () => {
       {new Array(30).fill(" ").map(() => {
         return (
           <div style={{ marginBottom: 200 }}>
-            <Sandpack options={{ initMode: "user-visible" }} />
+            <Sandpack
+              options={{
+                initMode: "user-visible",
+                initModeObserverOptions: { rootMargin: "2000px 0px" },
+              }}
+            />
           </div>
         );
       })}
