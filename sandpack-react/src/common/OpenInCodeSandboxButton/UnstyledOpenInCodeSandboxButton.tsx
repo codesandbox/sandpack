8910df280050fcd56a84755a4761248da7cb2443
Sophie Alpert <git@sophiebits.com>
2022-02-04 14:07:36 -0800

    OpenInCodeSandboxButton: preserve ?file= param for big sandboxes
    Ensure that the current file stays selected even if the URL is too long and we switch to a form.

--- sandpack-react/src/common/OpenInCodeSandboxButton/UnstyledOpenInCodeSandboxButton.tsx
+++ sandpack-react/src/common/OpenInCodeSandboxButton/UnstyledOpenInCodeSandboxButton.tsx
@@ -62,6 +62,14 @@ export const UnstyledOpenInCodeSandboxButton: React.FC<
     sandpack.openInCSBRegisteredRef.current = true;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  
+  const searchParams = new URLSearchParams({
+    parameters: paramsValues,
+    query: new URLSearchParams({
+      file: sandpack.activePath,
+      'from-sandpack': true,
+    }).toString(),
+  });
 
   /**
    * This is a safe limit to avoid too long requests (401),
@@ -75,7 +83,10 @@ export const UnstyledOpenInCodeSandboxButton: React.FC<
         {...props}
       >
         <form ref={formRef} action={CSB_URL} method="POST" target="_blank">
-          <input name="parameters" type="hidden" value={paramsValues} />
+          {Array.from(
+            searchParams,
+            ([k, v]) => <input key={k} name={k} type="hidden" value={v} />,
+          )}
         </form>
         {children}
       </button>
@@ -84,7 +95,7 @@ export const UnstyledOpenInCodeSandboxButton: React.FC<
 
   return (
     <a
-      href={`${CSB_URL}?parameters=${paramsValues}&query=file=${sandpack.activePath}%26from-sandpack=true`}
+      href={`${CSB_URL}?${searchParams.toString()}`}
       rel="noreferrer noopener"
       target="_blank"
       title="Open in CodeSandbox"
