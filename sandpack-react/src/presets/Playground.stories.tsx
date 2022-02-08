517f6bdf610216844208e637b39cf9efc123389b
Danilo Woznica <danilowoz@gmail.com>
2022-02-08 11:15:52 +0000

    fix(codemirror instance): refresh instance on rerender

--- sandpack-react/src/presets/Playground.stories.tsx
+++ sandpack-react/src/presets/Playground.stories.tsx
@@ -1,9 +1,43 @@
-import { Sandpack } from "../";
+import { useState, useEffect, useCallback } from "react";
+import { SandpackProvider, SandpackCodeEditor, SandpackLayout } from "../";
 
 export default {
   title: "Playground",
 };
 
+const Editor = () => {
+  const [editorInstance, setEditorInstance] = useState(null);
+
+  useEffect(() => {
+    if (!editorInstance) return;
+
+    const onFocus = () => console.log("onFocus");
+    const onBlur = () => console.log("onBlur");
+
+    editorInstance.contentDOM.addEventListener("focus", onFocus);
+    editorInstance.contentDOM.addEventListener("blur", onBlur);
+
+    return () => {
+      editorInstance.contentDOM.removeEventListener("focus", onFocus);
+      editorInstance.contentDOM.removeEventListener("blur", onBlur);
+    };
+  }, [editorInstance]);
+
+  const setRef = useCallback((node) => {
+    if (node?.getCodemirror) {
+      setEditorInstance(node.getCodemirror());
+    }
+  }, []);
+
+  return <SandpackCodeEditor ref={setRef} />;
+};
+
 export const Main = (): JSX.Element => {
-  return <Sandpack />;
+  return (
+    <SandpackProvider>
+      <SandpackLayout>
+        <Editor />
+      </SandpackLayout>
+    </SandpackProvider>
+  );
 };
