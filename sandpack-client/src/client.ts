665b452a0b1a4c03a223ba7740ebec4b8244e9ee
Danilo Woznica <danilowoz@gmail.com>
2022-03-01 17:55:36 +0000

    Update client.ts

--- sandpack-client/src/client.ts
+++ sandpack-client/src/client.ts
@@ -1,7 +1,6 @@
 import { getTemplate } from "codesandbox-import-utils/lib/create-sandbox/templates";
 import isEqual from "lodash.isequal";
 
-
 import Protocol from "./file-resolver-protocol";
 import { IFrameProtocol } from "./iframe-protocol";
 import type {
