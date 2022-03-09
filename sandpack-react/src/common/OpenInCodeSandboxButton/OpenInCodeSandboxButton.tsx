75fe4e8307536d9b8cd2cdbc0b22b7ef5176a3b9
Danilo Woznica <danilowoz@gmail.com>
2022-03-09 10:16:56 +0000

    update icons

--- sandpack-react/src/common/OpenInCodeSandboxButton/OpenInCodeSandboxButton.tsx
+++ sandpack-react/src/common/OpenInCodeSandboxButton/OpenInCodeSandboxButton.tsx
@@ -1,7 +1,6 @@
 import { useClasser } from "@code-hike/classer";
 import * as React from "react";
 
-import { useSandpackTheme } from "../../hooks/useSandpackTheme";
 import { CSBIcon } from "../../icons";
 import { isDarkColor } from "../../utils/stringUtils";
 
@@ -11,17 +10,10 @@ import { UnstyledOpenInCodeSandboxButton } from "./UnstyledOpenInCodeSandboxButt
  * @category Components
  */
 export const OpenInCodeSandboxButton = (): JSX.Element | null => {
-  const { theme } = useSandpackTheme();
   const c = useClasser("sp");
 
-  const csbIconClass = isDarkColor(theme.palette.defaultBackground)
-    ? "csb-icon-dark"
-    : "csb-icon-light";
-
   return (
-    <UnstyledOpenInCodeSandboxButton
-      className={c("button", "icon-standalone", csbIconClass)}
-    >
+    <UnstyledOpenInCodeSandboxButton className={c("button", "icon-standalone")}>
       <CSBIcon />
     </UnstyledOpenInCodeSandboxButton>
   );
