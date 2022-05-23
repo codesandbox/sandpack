3edcb4d11238f47ecbc286a8535205579856d3f3
Danilo Woznica <danilowoz@gmail.com>
2022-05-23 10:04:26 +0100

    fix(sandpack messages): add console type

--- sandpack-client/src/types.ts
+++ sandpack-client/src/types.ts
@@ -200,4 +200,12 @@ export type SandpackMessage = BaseSandpackMessage &
     | {
         type: "activate-react-devtools";
       }
+    | {
+        type: "console";
+        logs: Array<{
+          method: string;
+          id: string;
+          data: string[];
+        }>;
+      }
   );
