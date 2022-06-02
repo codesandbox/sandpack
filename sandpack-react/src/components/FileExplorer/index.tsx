230d4436dd07be9c4aff7de0a57ccdd117a2810a
jerrywu <jerrywu001@gmail.com>
2022-06-02 15:56:26 +0800

    fix 482: hidden-files-in-explorer

--- sandpack-react/src/components/FileExplorer/index.tsx
+++ sandpack-react/src/components/FileExplorer/index.tsx
@@ -12,19 +12,31 @@ const fileExplorerClassName = css({
   height: "100%",
 });
 
+export interface SandpackFileExplorerProp {
+  /**
+   * enable auto hidden file in file explorer
+   * 
+   * @description set with hidden property in files property
+   * @default false
+   */
+  enableAutoHiddenFile?: boolean;
+}
+
 /**
  * @category Components
  */
 export const SandpackFileExplorer = ({
   className,
+  enableAutoHiddenFile = false,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>): JSX.Element | null => {
+}: SandpackFileExplorerProp & React.HTMLAttributes<HTMLDivElement>): JSX.Element | null => {
   const { sandpack } = useSandpack();
 
   return (
     <div className={classNames(fileExplorerClassName, className)} {...props}>
       <ModuleList
         activeFile={sandpack.activeFile}
+        enableAutoHiddenFile={enableAutoHiddenFile}
         files={sandpack.files}
         prefixedPath="/"
         selectFile={sandpack.openFile}
