88cbc83797122a5241e6ee819255246064538f32
Danilo Woznica <danilowoz@gmail.com>
2022-08-11 10:07:18 +0100

    feat(themes): dracula 🧛 (#551)

--- sandpack-react/src/themes/ExternalThemes.stories.tsx
+++ sandpack-react/src/themes/ExternalThemes.stories.tsx
@@ -2,16 +2,38 @@ import * as allThemes from "@codesandbox/sandpack-themes";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 
-import { Sandpack } from "../";
+import {
+  Sandpack,
+  SandpackCodeEditor,
+  SandpackFileExplorer,
+  SandpackPreview,
+  SandpackProvider,
+  SandpackLayout,
+} from "../";
 
 const stories = storiesOf("presets/Themes (external)", module);
 
 Object.entries(allThemes).forEach(([themeName, value]) =>
   stories.add(themeName, () => (
-    <Sandpack
-      options={{ showInlineErrors: true }}
-      template="react"
-      theme={value}
-    />
+    <>
+      <Sandpack
+        options={{
+          showLineNumbers: true,
+          showInlineErrors: true,
+          showNavigator: true,
+          showTabs: true,
+        }}
+        template="react"
+        theme={value}
+      />
+
+      <SandpackProvider template="react" theme={value}>
+        <SandpackLayout>
+          <SandpackFileExplorer />
+          <SandpackCodeEditor showLineNumbers showTabs />
+          <SandpackPreview showNavigator />
+        </SandpackLayout>
+      </SandpackProvider>
+    </>
   ))
 );
