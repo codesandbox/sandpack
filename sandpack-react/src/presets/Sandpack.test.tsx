9969bf930912885fafc5c272a42b0001305683ed
Danilo Woznica <danilowoz@gmail.com>
2022-08-30 09:18:57 +0000

    Update Sandpack.test.tsx and styles.test.ts.snap

--- sandpack-react/src/presets/Sandpack.test.tsx
+++ sandpack-react/src/presets/Sandpack.test.tsx
@@ -33,7 +33,7 @@ describe("getSandpackCssText", () => {
       </SandpackProvider>
     );
 
-    expect(getSandpackCssText().length).toBe(3600);
+    expect(getSandpackCssText().length).toBe(3590);
     expect(getSandpackCssText()).not.toContain(componentClassName);
   });
 
