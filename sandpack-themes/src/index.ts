f48dc93ff3e213e3c19aa63b10161378835ee74b
Wachirawitch <57826174+ercusz@users.noreply.github.com>
2022-10-04 15:41:42 +0700

    feat(theme): atom dark ⚛ (#598)

--- sandpack-themes/src/index.ts
+++ sandpack-themes/src/index.ts
@@ -1,6 +1,7 @@
 export { aquaBlue } from "./aquaBlue";
+export { atomDark } from "./atomDark";
+export { dracula } from "./dracula";
 export { githubLight } from "./githubLight";
 export { monokaiPro } from "./monokaiPro";
 export { nightOwl } from "./nightOwl";
 export { sandpackDark } from "./sandpackDark";
-export { dracula } from "./dracula";
