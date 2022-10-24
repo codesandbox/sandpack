b9695ee8dd3719711beb43a30030c8ad9019e1fc
Danilo Woznica <danilowoz@gmail.com>
2022-10-24 19:44:56 +0000

    Update 3 files

---
+++ sandpack-react/out.js
@@ -0,0 +1,6 @@
+import { Sandpack } from "./src";
+const main = () => {
+  const foo = /* @__PURE__ */ React.createElement(Sandpack, null);
+  console.log(foo);
+};
+main();
---
+++ sandpack-react/ssr.tsx
@@ -0,0 +1,9 @@
+import { Sandpack } from "./src";
+
+const main = (): void => {
+  const foo = <Sandpack />;
+
+  console.log(foo);
+};
+
+main();
