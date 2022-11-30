2641325de736bd4ecf7f79890134344ecea21d59
Danilo Woznica <danilowoz@gmail.com>
2022-11-30 09:48:37 +0000

    Update .bundler and Templates.stories.tsx

--- sandpack-react/src/templates/Templates.stories.tsx
+++ sandpack-react/src/templates/Templates.stories.tsx
@@ -10,9 +10,6 @@ const stories = storiesOf("presets/Template", module);
 
 Object.keys(SANDBOX_TEMPLATES).forEach((template) =>
   stories.add(template, () => (
-    <Sandpack
-      options={{ bundlerURL: "http://localhost:3000/" }}
-      template={template as SandpackPredefinedTemplate}
-    />
+    <Sandpack template={template as SandpackPredefinedTemplate} />
   ))
 );
