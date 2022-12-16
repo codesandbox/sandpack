22726911e18c551125f7900be52e8671a6db8ca5
jerrywu001 <57242263@163.com>
2022-12-16 09:04:45 +0800

    update: auto predefined template keys

--- sandpack-react/src/types.ts
+++ sandpack-react/src/types.ts
@@ -232,18 +232,7 @@ export type SandboxEnvironment =
 /**
  * @category Setup
  */
-export type SandpackPredefinedTemplate =
-  | "angular"
-  | "react"
-  | "react-ts"
-  | "test-ts"
-  | "vanilla"
-  | "vanilla-ts"
-  | "vue"
-  | "vue3"
-  | "vue3-ts"
-  | "svelte"
-  | "solid";
+export type SandpackPredefinedTemplate = keyof typeof SANDBOX_TEMPLATES;
 
 /**
  * @category Setup
