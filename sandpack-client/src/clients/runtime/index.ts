f1ab8675122c44a54fc7bb910d43b67facd5d176
Danilo Woznica <danilowoz@gmail.com>
2023-02-20 15:48:36 +0000

    linting

--- sandpack-client/src/clients/runtime/index.ts
+++ sandpack-client/src/clients/runtime/index.ts
@@ -233,7 +233,7 @@ export class SandpackRuntime extends SandpackClient {
         this.sandboxSetup.disableDependencyPreprocessing,
       template:
         this.sandboxSetup.template ||
-        getTemplate({ pkg: packageJSON, modules: normalizedModules }),
+        getTemplate(packageJSON, normalizedModules),
       showOpenInCodeSandbox: this.options.showOpenInCodeSandbox ?? true,
       showErrorScreen: this.options.showErrorScreen ?? true,
       showLoadingScreen: this.options.showLoadingScreen ?? true,
