29780a2dc731c82dd7cacd4c91d93beac384761c
Jasper De Moor <jasperdemoor@gmail.com>
2023-02-22 16:07:43 +0000

    Update consoleHook.ts

--- sandpack-client/src/clients/node/inject-scripts/consoleHook.ts
+++ sandpack-client/src/clients/node/inject-scripts/consoleHook.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment,no-console,@typescript-eslint/explicit-function-return-type, no-restricted-globals */
 import Hook from "console-feed/lib/Hook";
-import { Decode } from "console-feed/lib/Transform";
+import { Encode } from "console-feed/lib/Transform";
 
 Hook(window.console, (log) => {
   parent.postMessage(
     {
       type: "console",
       codesandbox: true,
-      log: Decode(log),
+      log: Encode(log),
     },
     "*"
   );
