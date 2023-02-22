89402f20ed57eff7c239bca376b36de02107b4b0
Danilo Woznica <danilowoz@gmail.com>
2023-02-22 16:55:20 +0000

    Update consoleHook.ts and useSandpackConsole.ts

--- sandpack-client/src/clients/node/inject-scripts/consoleHook.ts
+++ sandpack-client/src/clients/node/inject-scripts/consoleHook.ts
@@ -3,11 +3,12 @@ import Hook from "console-feed/lib/Hook";
 import { Encode } from "console-feed/lib/Transform";
 
 Hook(window.console, (log) => {
+  const encodeMessage = Encode(log) as any;
   parent.postMessage(
     {
       type: "console",
       codesandbox: true,
-      log: Encode(log),
+      log: Array.isArray(encodeMessage) ? encodeMessage[0] : encodeMessage,
     },
     "*"
   );
