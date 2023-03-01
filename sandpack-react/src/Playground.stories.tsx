3e503a2ffbff3870b6f6ee3082e1ef7d2866b69f
Danilo Woznica <danilowoz@gmail.com>
2023-03-01 13:25:52 +0000

    Update Playground.stories.tsx

--- sandpack-react/src/Playground.stories.tsx
+++ sandpack-react/src/Playground.stories.tsx
@@ -9,15 +9,35 @@ import {
 } from "./components";
 
 import { SandpackProvider } from ".";
+import { useSandpack } from "./hooks";
 
 export default {
   title: "Intro/Playground",
+};
+
+const AddFile = () => {
+  const { sandpack } = useSandpack();
+  return (
+    <button
+      onClick={() => {
+        sandpack.addFile(
+          "/pages/foo/index.jsx",
+          `export default () => {
+  return "foo"
+}`
+        );
+      }}
+    >
+      Add file
+    </button>
+  );
 };
 
 export const Basic: React.FC = () => {
   return (
     <>
       <SandpackProvider template="nextjs" theme={themes.sandpackDark}>
+        <AddFile />
         <SandpackFileExplorer />
         <SandpackCodeEditor />
         <SandpackPreview />
