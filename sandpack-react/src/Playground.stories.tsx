bf6e4283c4402d8e5511dc633525b7c2e96e312f
Danilo Woznica <danilowoz@gmail.com>
2023-03-01 09:33:03 +0000

    Update Playground.stories.tsx

--- sandpack-react/src/Playground.stories.tsx
+++ sandpack-react/src/Playground.stories.tsx
@@ -1,31 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as themes from "@codesandbox/sandpack-themes";
 import React from "react";
-
-import { Sandpack } from "./presets";
+import { SandpackProvider } from ".";
+import {
+  SandpackCodeEditor,
+  SandpackFileExplorer,
+  SandpackPreview,
+} from "./components";
+import { useSandpack } from "./hooks";
 
 export default {
   title: "Intro/Playground",
 };
 
+const AddFile = () => {
+  const { sandpack } = useSandpack();
+  return (
+    <button
+      onClick={() => {
+        sandpack.addFile("/pages/foo.jsx", "()=> {}");
+      }}
+    >
+      Add file
+    </button>
+  );
+};
+
 export const Basic: React.FC = () => {
-  const [text, setText] = React.useState("world");
   return (
     <>
-      <input
-        defaultValue={text}
-        onChange={({ target }) => setText(target.value)}
-      />
-      <Sandpack
-        files={{
-          "App.js": `export default function App() {
-  return <h1>Hello ${text}</h1>
-}
-`,
-        }}
-        template="react"
-        theme={themes.sandpackDark}
-      />
+      <SandpackProvider template="nextjs" theme={themes.sandpackDark}>
+        <AddFile />
+        <SandpackFileExplorer />
+        <SandpackCodeEditor />
+        <SandpackPreview />
+      </SandpackProvider>
     </>
   );
 };
