16f86843c5d825a4e9f8bf03e94c8c2e070aab52
Danilo Woznica <danilowoz@gmail.com>
2023-03-02 10:29:35 +0000

    fix(nodebox): increase timeout

--- sandpack-react/src/Playground.stories.tsx
+++ sandpack-react/src/Playground.stories.tsx
@@ -2,23 +2,12 @@
 import * as themes from "@codesandbox/sandpack-themes";
 import React from "react";
 
-import {
-  SandpackCodeEditor,
-  SandpackFileExplorer,
-  SandpackPreview,
-} from "./components";
-import { useSandpack } from "./hooks";
-
-import { SandpackProvider } from ".";
+import { Sandpack } from "./";
 
 export default {
   title: "Intro/Playground",
 };
 
 export const Basic: React.FC = () => {
-  return (
-    <>
-      <Sandpack template="nextjs" theme={themes.sandpackDark} />
-    </>
-  );
+  return <Sandpack template="nextjs" theme={themes.sandpackDark} />;
 };
