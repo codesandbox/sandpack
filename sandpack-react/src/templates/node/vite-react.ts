f7263ce1ca7b0e80674789efa5a17bd25a5601e1
Danilo Woznica <danilowoz@gmail.com>
2023-03-06 14:48:23 +0000

    fix(template vite): clean cache on start script (#806)

--- sandpack-react/src/templates/node/vite-react.ts
+++ sandpack-react/src/templates/node/vite-react.ts
@@ -43,7 +43,7 @@ root.render(
     "/package.json": {
       code: JSON.stringify({
         scripts: {
-          dev: "vite",
+          dev: "vite --force",
           build: "vite build",
           preview: "vite preview",
         },
