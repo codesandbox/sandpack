7f611f077acb3aac33575eea0b807f7a44b5bbe5
Utwo <mihai.legat@gmail.com>
2023-04-04 16:05:50 +0300

    Add rewrites for docs and themes.

--- website/landing/next.config.js
+++ website/landing/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/docs",
+        destination: "https://sandpack.vercel.app/docs",
+      },
+      {
+        source: "/theme",
+        destination: "https://sandpack-theme.vercel.app/theme",
+      },
+      {
+        source: "/docs/:path*",
+        destination: "https://sandpack.vercel.app/docs/:path*",
+      },
+      {
+        source: "/theme/:path*",
+        destination: "https://sandpack-theme.vercel.app/theme/:path*",
+      },
+    ];
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.woff/,
