755b6727fb74745b70ecd0bbd89c2c2e700141e7
SSHari <thesshguy@gmail.com>
2023-04-15 14:37:04 -0400

    feat: add console hook to static template

--- sandpack-client/src/clients/static/index.ts
+++ sandpack-client/src/clients/static/index.ts
@@ -15,6 +15,10 @@ import type { SandpackNodeMessage } from "../node/types";
 
 import { insertHtmlAfterRegex, readBuffer, validateHtml } from "./utils";
 
+// get the bundled file, which contains all dependencies
+// @ts-ignore
+import consoleHook from "../../inject-scripts/dist/consoleHook.js";
+
 export class SandpackStatic extends SandpackClient {
   private emitter: EventEmitter;
   private previewController: PreviewController;
@@ -50,6 +54,9 @@ export class SandpackStatic extends SandpackClient {
               content,
               options.externalResources
             );
+            content = this.injectScriptIntoHead(content, {
+              script: consoleHook,
+            });
           } catch (err) {
             console.error("Runtime injection failed", err);
           }
@@ -78,6 +85,11 @@ export class SandpackStatic extends SandpackClient {
         "allow",
         "accelerometer; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; clipboard-write;"
       );
+    }
+
+    this.eventListener = this.eventListener.bind(this);
+    if (typeof window !== "undefined") {
+      window.addEventListener("message", this.eventListener);
     }
 
     // Dispatch very first compile action
@@ -137,6 +149,21 @@ export class SandpackStatic extends SandpackClient {
     return this.injectContentIntoHead(content, tagsToInsert);
   }
 
+  private injectScriptIntoHead(
+    content: FileContent,
+    opts: { script: string; scope?: Record<string, any> }
+  ): FileContent {
+    const { script, scope = {} } = opts;
+    const scriptToInsert = `
+    <script>
+      const scope = ${JSON.stringify(scope)};
+      ${script}
+    </script>
+    `.trim();
+
+    return this.injectContentIntoHead(content, scriptToInsert);
+  }
+
   public updateSandbox(
     setup = this.sandboxSetup,
     _isInitializationCompile?: boolean
@@ -169,6 +196,21 @@ export class SandpackStatic extends SandpackClient {
     });
   }
 
+  // Handles message windows coming from iframes
+  private eventListener(evt: MessageEvent): void {
+    // skip events originating from different iframes
+    if (evt.source !== this.iframe.contentWindow) {
+      return;
+    }
+
+    const message = evt.data;
+    if (!message.codesandbox) {
+      return;
+    }
+
+    this.dispatch(message);
+  }
+
   /**
    * Bundler communication
    */
@@ -190,5 +232,8 @@ export class SandpackStatic extends SandpackClient {
 
   public destroy(): void {
     this.emitter.cleanup();
+    if (typeof window !== "undefined") {
+      window.removeEventListener("message", this.eventListener);
+    }
   }
 }
