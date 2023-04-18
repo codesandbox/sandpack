bcfb2baa8efc0789fc443df735684ecb42fd4817
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-04-18 16:31:18 +0200

    docs: added nextjs app dir example (#910)

---
+++ examples/nextjs-app-dir/app/page.tsx
@@ -0,0 +1,9 @@
+import { SandpackExamples } from "@/components/sandpack-examples";
+
+export default function Home() {
+  return (
+    <div>
+      <SandpackExamples />
+    </div>
+  );
+}
---
+++ examples/nextjs-app-dir/components/sandpack-styles.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { getSandpackCssText } from "@codesandbox/sandpack-react";
+import { useServerInsertedHTML } from "next/navigation";
+
+/**
+ * Ensures CSSinJS styles are loaded server side.
+ */
+export const SandPackCSS = () => {
+  useServerInsertedHTML(() => {
+    return (
+      <style
+        dangerouslySetInnerHTML={{ __html: getSandpackCssText() }}
+        id="sandpack"
+      />
+    );
+  });
+  return null;
+};
