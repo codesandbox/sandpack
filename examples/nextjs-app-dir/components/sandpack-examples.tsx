bfb483c56063eb124a7d69aa0e1aba307a82859f
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-04-18 10:45:11 +0200

    Added nextjs app dir example

---
+++ examples/nextjs-app-dir/components/sandpack-examples.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Sandpack } from "@codesandbox/sandpack-react";
+import { githubLight, sandpackDark } from "@codesandbox/sandpack-themes";
+/**
+ * The only reason this is a separate import, is so
+ * we don't need to make the full page 'use client', but only this copmponent.
+ */
+export const SandpackExamples = () => {
+  return (
+    <>
+      <Sandpack />
+      <Sandpack theme="dark" />
+      <Sandpack theme={githubLight} template="nextjs" />
+      <Sandpack options={{ readOnly: true }} theme={sandpackDark} />
+    </>
+  );
+};
