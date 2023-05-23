ad386bf673266c7b639ee11693b59e2413e18ff2
Filipe Lima <filipe@codesandbox.io>
2023-05-23 10:51:10 +0000

    feat(docs): integrate Amplitude analytics to track pageviews

---
+++ website/docs/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import amplitude from "amplitude-js";
+import { useEffect } from "react";
+
+const API_KEY = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY;
+
+if (API_KEY && process.browser) {
+  amplitude.getInstance().init(API_KEY);
+}
+
+function MyApp({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  useEffect(() => {
+    amplitude.getInstance().logEvent("Pageview");
+  }, []);
+
+  return getLayout(<Component {...pageProps} />);
+}
+
+export default MyApp;
