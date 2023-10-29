1ebd4cd71360261dc1d505d5ec1dcb883cac5d97
Ives van Hoorne <ives@codesandbox.io>
2023-10-29 16:51:36 +0100

    chore: update storybook to v7

--- sandpack-react/.storybook/main.cjs
+++ sandpack-react/.storybook/main.cjs
@@ -1,4 +1,22 @@
+const { dirname, join } = require("path");
+
 module.exports = {
-  core: { builder: "@storybook/builder-vite" },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+
+  docs: {
+    autodocs: true,
+  },
+
+  features: {
+    storyStoreV7: false, // 👈 Opt out of on-demand story loading
+  },
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
