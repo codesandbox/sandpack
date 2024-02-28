230960c1dafd74d7e5e578865cb0b27526152e07
Danilo Woznica <danilowoz@gmail.com>
2024-02-28 12:04:35 +0000

    fix: make sure to unregister all clients when "user-visible" option is out of viewport (#1098)

--- sandpack-react/src/Playground.stories.tsx
+++ sandpack-react/src/Playground.stories.tsx
@@ -11,6 +11,25 @@ export const Basic: React.FC = () => {
   return (
     <div style={{ height: "400vh" }}>
       <Sandpack
+        files={{
+          "/App.js": `export default function TodoList() {
+            return (
+              // This doesn't quite work!
+              <h1>Hedy Lamarr's Todos</h1>
+              <img
+                src="https://i.imgur.com/yXOvdOSs.jpg"
+                alt="Hedy Lamarr"
+                class="photo"
+              >
+              <ul>
+                <li>Invent new traffic lights
+                <li>Rehearse a movie scene
+                <li>Improve spectrum technology
+              </ul>
+            );
+          }
+          `,
+        }}
         options={{
           initMode: "user-visible",
           bundlerURL: "https://786946de.sandpack-bundler-4bw.pages.dev",
