c3bc09d11a0cf492c5211bf25d5916685aeae170
Danilo Woznica <danilowoz@gmail.com>
2024-06-06 12:01:29 +0100

    fix(export): normalize files and pass template (#1145)

--- sandpack-react/src/PrivatePackage.stories.tsx
+++ sandpack-react/src/PrivatePackage.stories.tsx
@@ -20,8 +20,6 @@ export default function App() {
   return <Button>Hello World</Button>
 }`,
         }}
-        // options={{ bundlerURL: `http://localhost:3000` }}
-        // options={{ bundlerURL: `https://2-1-0-sandpack.codesandbox.stream/` }}
         teamId="642af90c-4717-4730-bad3-e4c1e37ca5e2"
         template="react"
       />
