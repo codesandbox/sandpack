58d1f54a1f18fb917b54510976f5ab87c4d16ae7
Danilo Woznica <danilowoz@gmail.com>
2024-09-11 09:13:08 +0100

    fix(file-tab): close button (#1198)
    * fix(file-tab): close button
    
    * fix
    
    * fix

--- sandpack-react/src/Playground.stories.tsx
+++ sandpack-react/src/Playground.stories.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 
-import { Sandpack } from "./";
+import {
+  SandpackCodeEditor,
+  SandpackFileExplorer,
+  SandpackLayout,
+  SandpackProvider,
+} from "./";
 
 export default {
   title: "Intro/Playground",
@@ -10,28 +15,12 @@ export default {
 export const Basic: React.FC = () => {
   return (
     <div style={{ height: "400vh" }}>
-      <Sandpack
-        options={{
-          showTabs: true,
-          closableTabs: true,
-          experimental_enableServiceWorker: true,
-          experimental_enableStableServiceWorkerId: true,
-        }}
-        // customSetup={{
-        //   dependencies: {
-        //     "react-content-loader": "latest",
-        //     "radix-ui": "latest",
-        //     "styled-components": "latest",
-        //     "react-dom": "latest",
-        //     react: "latest",
-        //     "react-table": "latest",
-        //   },
-        // }}
-        // options={{
-        //   bundlerURL: "https://ymxnqs-3000.csb.app",
-        // }}
-        template="react"
-      />
+      <SandpackProvider>
+        <SandpackLayout>
+          <SandpackFileExplorer />
+          <SandpackCodeEditor closableTabs showTabs />
+        </SandpackLayout>
+      </SandpackProvider>
     </div>
   );
 };
